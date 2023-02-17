e96e135121b837a465f94057d222ba135ae86df8
AndyLi23 <andyliqy@gmail.com>
2023-02-17 11:51:31 -0800

    camera

---
+++ camera.py
@@ -0,0 +1,65 @@
+from threading import Thread
+import subprocess
+import numpy as np
+
+res = (1280, 800)
+
+args = [
+        "ffmpeg",
+        "-i",
+        "",
+        "-f",
+        "image2pipe",
+        "-pix_fmt",
+        "rgb24",
+        "-vcodec",
+        "rawvideo",
+        "-",
+    ]
+
+class Stream():
+    def __init__(self, src):
+        
+        args[2] = "/dev/video" + str(src)
+        
+        self.pipe = subprocess.Popen(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            bufsize=res[0] * res[1] * 3,
+        )
+        
+        self.frame = None
+        self.switch = False
+        self.src = src
+        
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+        
+    def update(self):
+        while True:
+            re = self.pipe.stdout.read(res[0] * res[1] * 3)
+            if len(re) > 0:
+                array = np.frombuffer(re, dtype="uint8")
+                self.frame = array.reshape((res[1], res[0], 3))
+                
+            if self.switch:
+                self.pipe.stdout.close()
+                args[2] = "/dev/video" + str(self.src)
+                self.switch = False
+                
+                self.pipe = subprocess.Popen(
+                    args,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.DEVNULL,
+                    bufsize=res[0] * res[1] * 3,
+                )
+                
+    def switch_cam(self, src):
+        self.src = src
+        self.switch = True
+            
+    def get(self):
+        return self.frame
+            
